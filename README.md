892ca9a47e6a43602f86359505d53df16f250314
msm1984 <sadeghmomo2020gmail.com>
2023-01-20 02:11:14 +0330

    part2 finished

---
+++ README.md
@@ -0,0 +1,6 @@
+# GitHub-Programming-Languages-Data
+A common question for those new and familiar to computer science and software engineering is what is the most best and/or most popular programming language. It is very difficult to give a definitive answer, as there are a seemingly indefinite number of metrics that can define the 'best' or 'most popular' programming language.
+
+One such metric that can be used to define a 'popular' programming language is the number of projects and files that are made using that programming language. As GitHub is the most popular public collaboration and file-sharing platform, analyzing the languages that are used for repositories, PRs, and issues on GitHub and be a good indicator for the popularity of a language.
+
+This dataset contains statistics about the programming languages used for repositories, PRs, and issues on GitHub. The data is from 2011 to 2021.
---
+++ part_2/part_2.R
@@ -0,0 +1,40 @@
+mydata = read.csv("C:\\Users\\montazeri\\Desktop\\amar\\used_device_data.csv")
+#محاسبه بازه 95 درصدی برای میانگین
+#محاسبه میانگین،تعداد اعضا و انحراف معیار
+tmp = mydata$battery
+baze_mian.mean = mean(tmp,na.rm = T)
+baze_mian.num = length(tmp)
+baze_mian.sd = sd(tmp,na.rm = T)
+baze_mian.var = var(tmp,na.rm = T)
+baze_mian.se = baze_mian.sd/sqrt(baze_mian.num)
+baze_mian.se
+alpha = 0.05
+degrees.freedom = baze_mian.num - 1
+#محاسبه بازه 95 درصدی میانگین جمعیت
+#محاسبه بازه 95 درصدی با استفاده از توزیع نرمال
+error = qnorm(1-(alpha/2))*baze_mian.se
+lower.bound = baze_mian.mean - error
+upper.bound = baze_mian.mean + error
+cat("بازه اطمینان 95 درصدی با استفاده از توزیع نورمال:",c(lower.bound,upper.bound))
+#T محاسبه بازه 95 درصدی با استفاده از توزیع
+error.t = qt(1-(alpha/2),degrees.freedom)*baze_mian.se
+lower.t.bound = baze_mian.mean - error.t
+upper.t.bound = baze_mian.mean + error.t
+#محاسبه بازه 95 درصدی واریانس جمعیت 
+#محاسبه بازه 95 درصدی با استفاده از توزیع مربع کای
+error.chi.lower = degrees.freedom*baze_mian.var/qchisq(1-(alpha/2),degrees.freedom)
+error.chi.upper = degrees.freedom*baze_mian.var/qchisq(alpha/2,degrees.freedom)
+#انجام آزمون آماری
+komaki = mydata$weight
+n = length(komaki)
+avg = mean(komaki,na.rm = T)
+sd = sd(komaki,na.rm = T)
+alpha = 0.05
+mu0 = avg + 10
+zt = (avg-mu0)/(sd/sqrt(n))
+zt1 = qt((1-alpha),n-1,lower.tail = T)
+if (zt > zt1){
+  cat("فرض صفرم رد می شود")
+} else{
+  cat("فرض صفرم رد نمی شود")
+}
