f936d859fa46cd435363e7cc4d301d93a9257429
msm1984 <sadeghmomo2020gmail.com>
2023-01-20 02:30:22 +0330

    final version

--- part_2/part_2.R
+++ part_2/part_2.R
@@ -15,24 +15,31 @@ degrees.freedom = baze_mian.num - 1
 error = qnorm(1-(alpha/2))*baze_mian.se
 lower.bound = baze_mian.mean - error
 upper.bound = baze_mian.mean + error
-cat("بازه اطمینان 95 درصدی با استفاده از توزیع نورمال:",c(lower.bound,upper.bound))
+#چاپ بازه 95 درصدی
+print(c(lower.bound,upper.bound))
 #T محاسبه بازه 95 درصدی با استفاده از توزیع
 error.t = qt(1-(alpha/2),degrees.freedom)*baze_mian.se
 lower.t.bound = baze_mian.mean - error.t
 upper.t.bound = baze_mian.mean + error.t
+#چاپ بازه 95 درصدی
+print(c(lower.t.bound,upper.t.bound))
 #محاسبه بازه 95 درصدی واریانس جمعیت 
 #محاسبه بازه 95 درصدی با استفاده از توزیع مربع کای
 error.chi.lower = degrees.freedom*baze_mian.var/qchisq(1-(alpha/2),degrees.freedom)
 error.chi.upper = degrees.freedom*baze_mian.var/qchisq(alpha/2,degrees.freedom)
+#چاپ بازه 95 درصدی
+print(c(error.chi.lower,error.chi.upper))
 #انجام آزمون آماری
 komaki = mydata$weight
 n = length(komaki)
 avg = mean(komaki,na.rm = T)
 sd = sd(komaki,na.rm = T)
-alpha = 0.05
+alpha1 = 0.05
 mu0 = avg + 10
 zt = (avg-mu0)/(sd/sqrt(n))
-zt1 = qt((1-alpha),n-1,lower.tail = T)
+zt1 = qt((1-alpha1),n-1,lower.tail = T)
+zt
+zt1
 if (zt > zt1){
   cat("فرض صفرم رد می شود")
 } else{
